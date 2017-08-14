f5c367f093d5da0d194b9f45dbff01a92ed0d333
developercyrus <developer.cyrus@gmail.com>
2017-08-14 16:30:51 +0800

    revised

--- remove.js
+++ remove.js
@@ -2,7 +2,8 @@ function remove() {
     try {
         //document.getElementById('pagelet_ego_pane_w').innerHTML = '';
         //var element = document.querySelector("span.gb_0a.gb_Za");
-    	var element = document.querySelector("span.gb_Za.gb_Za");
+    	//var element = document.querySelector("span.gb_Za.gb_Za");
+        var element = document.querySelector("span.gb_Xa.gb_Wa");
         element.parentElement.removeChild(element);
     }
     catch(err) {
