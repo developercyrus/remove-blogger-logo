bbe0d9cf3b65d1b4cb5cde1b468b8532038be07b
developercyrus <developer.cyrus@gmail.com>
2018-11-14 09:37:28 +0800

    upgraded to 1.3

--- remove.js
+++ remove.js
@@ -11,7 +11,8 @@ function remove() {
     	//var element = document.querySelector("div.gb_cc");
         //var element = document.querySelector("div.gb_Ac.gb_Ud");		// version 1.0
         //var element = document.querySelector("div.gb_Bc gb_Xd");		// version 1.1
-    	var element = document.querySelector("img.gb_Wa");				// version 1.2
+    	//var element = document.querySelector("img.gb_Wa");			// version 1.2
+        var element = document.querySelector("img.gb_Xa");				// version 1.3
         
         element.parentElement.removeChild(element);
     }
