a5fccd4e900a4e94cac7337c52aa7848ddbe7905
developercyrus <developer.cyrus@gmail.com>
2018-12-06 09:59:02 +0800

    upgraded to 1.5

--- remove.js
+++ remove.js
@@ -12,7 +12,8 @@ function remove() {
         //var element = document.querySelector("div.gb_Ac.gb_Ud");		// version 1.0
         //var element = document.querySelector("div.gb_Bc gb_Xd");		// version 1.1
     	//var element = document.querySelector("img.gb_Wa");			// version 1.2
-        var element = document.querySelector("img.gb_Xa");				// version 1.3
+        //var element = document.querySelector("img.gb_Xa");				// version 1.3
+        var element = document.querySelector("img.gb_Za");				// version 1.5
         
         element.parentElement.removeChild(element);
     }
