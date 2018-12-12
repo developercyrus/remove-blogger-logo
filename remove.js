62193841f5508d0f7747629706d2f0da211f74d5
developercyrus <developer.cyrus@gmail.com>
2018-12-12 09:52:15 +0800

    upgraded to 1.6

--- remove.js
+++ remove.js
@@ -9,11 +9,12 @@ function remove() {
     	//var element = document.querySelector("div.gb_zc.gb_Ud");
     	//var element = document.querySelector("div.gb_Cc.gb_Wd");    	
     	//var element = document.querySelector("div.gb_cc");
-        //var element = document.querySelector("div.gb_Ac.gb_Ud");		// version 1.0
-        //var element = document.querySelector("div.gb_Bc gb_Xd");		// version 1.1
-    	//var element = document.querySelector("img.gb_Wa");			// version 1.2
-        //var element = document.querySelector("img.gb_Xa");				// version 1.3
-        var element = document.querySelector("img.gb_Za");				// version 1.5
+        //var element = document.querySelector("div.gb_Ac.gb_Ud");      // version 1.0
+        //var element = document.querySelector("div.gb_Bc gb_Xd");      // version 1.1
+    	//var element = document.querySelector("img.gb_Wa");            // version 1.2
+        //var element = document.querySelector("img.gb_Xa");            // version 1.3
+        //var element = document.querySelector("img.gb_Za");            // version 1.5
+        var element = document.querySelector("img.gb_0a");              // version 1.6
         
         element.parentElement.removeChild(element);
     }
