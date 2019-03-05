c5e60a27662c2e43b34c6e59278cbae1ebb6582e
developercyrus <developer.cyrus@gmail.com>
2019-03-05 16:05:05 +0800

    version 1.8

--- remove.js
+++ remove.js
@@ -15,8 +15,7 @@ function remove() {
         //var element = document.querySelector("img.gb_Xa");            // version 1.3
         //var element = document.querySelector("img.gb_Za");            // version 1.5
         //var element = document.querySelector("img.gb_0a");            // version 1.6
-        var element = document.querySelector("img.gb_ma");              // version 1.7
-        gb_ma
+        var element = document.querySelector("img.gb_ma");              // version 1.8
         
         element.parentElement.removeChild(element);
     }
